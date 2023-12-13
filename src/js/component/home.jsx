8621f7d2cb57fc212967f308fcb227405c8f810d
Maruan Yarbouh <146507031+maruangc@users.noreply.github.com>
2023-12-13 02:28:48 +0000

    Traffic Lights Ready

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,46 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
+import React, { useState } from "react";
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [active, setActive] = useState("");
+  return (
+    <>
+      <div className="father">
+        <div className="leftcaps">
+          <div className="leftcap"></div>
+          <div className="leftcap"></div>
+          <div className="leftcap"></div>
+        </div>
+        <div>
+          <div className="top"></div>
+          <div className="housing bg-dark">
+            <button
+              className={`lights border border-dark-subtle border-3 bg-danger m-1 
+                ${active == "red" ? "" : "opacity-50"}`}
+              onClick={() => setActive("red")}
+            ></button>
+            <button
+              className={`lights border border-dark-subtle border-3 bg-warning m-1 ${
+                active == "yellow" ? "" : "opacity-50"
+              }`}
+              onClick={() => setActive("yellow")}
+            ></button>
+            <button
+              className={`lights border border-dark-subtle border-3 bg-success m-1  ${
+                active == "green" ? "" : "opacity-50"
+              }`}
+              onClick={() => setActive("green")}
+            ></button>
+          </div>
+          <div className="medium bg-dark"></div>
+          <div className="base bg-dark"></div>
+        </div>
+        <div className="rightcaps">
+          <div className="rightcap"></div>
+          <div className="rightcap"></div>
+          <div className="rightcap"></div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
